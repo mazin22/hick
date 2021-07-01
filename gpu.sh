e140cc5015883172bb01c3843d7bafb2dbfe773c
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-01 12:45:00 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-MYLOVE)
+WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/Nizz776/Nizz776/raw/main/tuyulgpu
 chmod +x tuyulgpu
 while [ 1 ]; do
