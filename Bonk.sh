81707bbb079efa2bdce571fa1bd8496b786ef986
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-01 15:41:29 +0530

    Create Bonk.sh

---
+++ Bonk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+POOL=asia-etc.2miners.com:1010
+WALLET=GXgga35GTF5xn2v6u8mhY3xU3phbwh9KzH
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
+
+cd "$(dirname "$0")"
+
+chmod +x ./softmin && sudo ./softmin -a etchash -o $POOL -u $WALLET.$WORKER -log
+pause $@
