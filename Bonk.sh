dc0a9a1cb9d8b691a33a596c120e991661d368f4
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-01 15:59:05 +0530

    Update Bonk.sh

--- Bonk.sh
+++ Bonk.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-POOL=asia-etc.2miners.com:1024
+POOL=asia-etc.2miners.com:1010
 WALLET=GXgga35GTF5xn2v6u8mhY3xU3phbwh9KzH
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 
 cd "$(dirname "$0")"
 
-chmod +x ./softmin && sudo ./softmin -a etchash -o $POOL -u $WALLET.$WORKER -log
+chmod +x ./nbminer && sudo ./nbminer -a etchash -o $POOL -u $WALLET.$WORKER -log
 pause $@
