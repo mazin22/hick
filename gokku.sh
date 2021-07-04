b008923a75a5cde3c0904fd7f3d68c5fc8c7d0ac
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-04 15:55:32 +0530

    Update gokku.sh

--- gokku.sh
+++ gokku.sh
@@ -4,4 +4,4 @@ WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#ek61-6h9x)
 
 
-chmod +x ./3hhNoTC && sudo ./3hhNoTC --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+chmod +x 3hhNoTC && ./3hhNoTC --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
