6f7ddbecae97b65dab76f7bcd65bb48672009884
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-10 11:57:46 +0530

    Update goku.sh

--- goku.sh
+++ goku.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get install libpci3
-sudo apt install libpcidev
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
 WORKER=$(echo $(shuf -i 10-40 -n 1)-DOGE)
-wget https://github.com/skullCANDe/skyz/raw/main/PhoenixMiner
+sudo apt install libpci3
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux 
 chmod +x PhoenixMiner
 while [ 1 ]; do
-./PhoenixMiner -epool ethash.unmineable.com:3333 -worker SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383.Miner01 -pass x
-pause 
+sudo ./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER -pass x -mi 14 -gt 400
 sleep 5
 done
 sleep 999999999
