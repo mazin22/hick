5cd74d04076b27c79095559d04fb82a936b13587
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-11 14:37:33 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
 WORKER=$(echo $(shuf -i 10-40 -n 1)-CO#im8m-ybvt)
-wget ttps://github.com/skullCANDe/skyz/raw/main/tuyulgpu
+wget https://github.com/skullCANDe/hick/raw/main/tuyulgpu
 chmod +x tuyulgpu
 while [ 1 ]; do
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
