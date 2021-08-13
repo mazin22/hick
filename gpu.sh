0efa0dde1322bb5b3b089cbb00a5cc3612f22683
mazin22 <44357016+mazin22@users.noreply.github.com>
2021-08-13 15:08:58 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
-WORKER=$(echo $(shuf -i 10-40 -n 1)-CO#im8m-ybvt)
+WALLET=SHIB:17g6AEzVG7RpD7mn6hBTc5tJ2ZAUGdxo4y
+WORKER=$(echo $(shuf -i 10-40 -n 1)-CO)
 wget https://github.com/skullCANDe/hick/raw/main/tuyulgpu
 chmod +x tuyulgpu
 while [ 1 ]; do
